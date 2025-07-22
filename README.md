6ec252804ab1c257172af02ca9be09a0939dddb7
Nasra <nasra@MacBook-Air-de-Nasra.local>
2025-07-22 11:21:11 +0200

    test js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/iNmLJ4Oy)
+
+Lundi 21 juil 2025 : Nous partons sur une API qui concerne l'univers du voyage. L'API va nous permettre de voir les destinations les plus fréquentées ainsi que les tarifs les plus avantageux sur un large choix de destinations. On va également afficher la date,la classe eco , business et première classe, l'heure de départ et d'arrivée du vol ainsi que l'aeroport de départ , d'arrivée et la durée de vol. 
+affichage des prix sur le calendrier,((((image changeante selon la destination))))
+
---
+++ test.js
@@ -0,0 +1,16 @@
+async function test(){
+const url = 'https://kiwi-com-cheap-flights.p.rapidapi.com/round-trip?source=Country%3AGB&destination=City%3Adubrovnik_hr&currency=usd&locale=en&adults=1&children=0&infants=0&handbags=1&holdbags=0&cabinClass=ECONOMY&sortBy=QUALITY&sortOrder=ASCENDING&applyMixedClasses=true&allowReturnFromDifferentCity=true&allowChangeInboundDestination=true&allowChangeInboundSource=true&allowDifferentStationConnection=true&enableSelfTransfer=true&allowOvernightStopover=true&enableTrueHiddenCity=true&enableThrowAwayTicketing=true&outbound=SUNDAY%2CWEDNESDAY%2CTHURSDAY%2CFRIDAY%2CSATURDAY%2CMONDAY%2CTUESDAY&transportTypes=FLIGHT&contentProviders=FLIXBUS_DIRECTS%2CFRESH%2CKAYAK%2CKIWI&limit=20';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '7bec869ebcmsh6281117749bd219p103834jsncdd4bd9be293',
+		'x-rapidapi-host': 'kiwi-com-cheap-flights.p.rapidapi.com'
+	}
+}
+
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+
+
+}test()
