399ebce389f00838dd62f5d0765218fcfbd28e8a
VincentComier <V.Comier@hotmail.fr>
2025-07-23 11:54:16 +0200

    fonction afficher prix

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
+let cout = document.getElementById("cout")
+let testeuh = document.getElementById("testeuh")
+
 async function test(){
 const url = 'https://kiwi-com-cheap-flights.p.rapidapi.com/round-trip?source=Country%3AFR&destination=Country%3Aanywhere&currency=eur&locale=en&adults=1&children=0&infants=0&handbags=1&holdbags=0&cabinClass=ECONOMY&sortBy=QUALITY&sortOrder=ASCENDING&applyMixedClasses=false&allowReturnFromDifferentCity=false&allowChangeInboundDestination=false&allowChangeInboundSource=false&allowDifferentStationConnection=false&enableSelfTransfer=false&allowOvernightStopover=false&enableTrueHiddenCity=false&enableThrowAwayTicketing=false&outbound=SUNDAY%2CWEDNESDAY%2CTHURSDAY%2CFRIDAY%2CSATURDAY%2CMONDAY%2CTUESDAY&transportTypes=FLIGHT&contentProviders=FLIXBUS_DIRECTS%2CFRESH%2CKAYAK%2CKIWI&limit=20&inboundDepartureDateStart=2025-12-01T00%3A00%3A00&inboundDepartureDateEnd=2026-01-01T00%3A00%3A00&outboundDepartmentDateStart=2025-08-01T00%3A00%3A00&outboundDepartmentDateEnd=2025-08-30T00%3A00%3A00';
 
@@ -37,49 +40,56 @@ iti.forEach((itinerary) => {
     const to = segment.destination.station.name;
     console.log(`  🛬 Segment retour ${i + 1} : ${from} → ${to}`);
 });
-
-console.log("Prix :", itinerary.price.amount + "€");
+let prix = itinerary.price.amount
+affichagePrix(prix)
+console.log("Prix :", prix + "€");
 console.log("-------------");
 console.log("MAXITEST" ,iti)
-});
+
+})
 }
 test()
+function affichagePrix(prix) {
+  let prices = document.createElement("p")
+  prices.innerHTML = prix
+  cout.appendChild(prices)
+} 
 
 
 
 
-// On récupère tous les itinéraires
-const itineraires = result.itineraries;
+// // On récupère tous les itinéraires
+// const itineraires = result.itineraries;
 
-// On parcourt chaque itinéraire un par un
-itineraires.forEach((itineraire) => {
-  console.log("✈️ Nouvel itinéraire :");
-   // --- VOLS ALLER ---
-  console.log("🛫 Vols aller :");
+// // On parcourt chaque itinéraire un par un
+// itineraires.forEach((itineraire) => {
+//   console.log("✈️ Nouvel itinéraire :");
+//    // --- VOLS ALLER ---
+//   console.log("🛫 Vols aller :");
 
-  const volsAller = itineraire.outbound.sectorSegments;
+//   const volsAller = itineraire.outbound.sectorSegments;
   
-  // On regarde chaque vol aller
-  volsAller.forEach((vol) => {
-    const depart = vol.segment.source.station.name;
-    const arrivee = vol.segment.destination.station.name;
-    console.log(`   De ${depart} vers ${arrivee}`);
-  });
-  // --- VOLS RETOUR ---
-  console.log("🛬 Vols retour :");
+//   // On regarde chaque vol aller
+//   volsAller.forEach((vol) => {
+//     const depart = vol.segment.source.station.name;
+//     const arrivee = vol.segment.destination.station.name;
+//     console.log(`   De ${depart} vers ${arrivee}`);
+//   });
+//   // --- VOLS RETOUR ---
+//   console.log("🛬 Vols retour :");
   
-  const volsRetour = itineraire.inbound.sectorSegments;
+//   const volsRetour = itineraire.inbound.sectorSegments;
   
-  // On regarde chaque vol retour
-  volsRetour.forEach((vol) => {
-    const depart = vol.segment.source.station.name;
-    const arrivee = vol.segment.destination.station.name;
-    console.log(`   De ${depart} vers ${arrivee}`);
-  });
+//   // On regarde chaque vol retour
+//   volsRetour.forEach((vol) => {
+//     const depart = vol.segment.source.station.name;
+//     const arrivee = vol.segment.destination.station.name;
+//     console.log(`   De ${depart} vers ${arrivee}`);
+//   });
 
-  // On affiche le prix
-  const prix = itineraire.price.amount;
+//   // On affiche le prix
+//   const prix = itineraire.price.amount;
 
-  console.log(`💰 Prix : ${prix}€`);
-  console.log("------------------------");
-});
+//   console.log(`💰 Prix : ${prix}€`);
+//   console.log("------------------------");
+// });
